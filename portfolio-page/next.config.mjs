2f6bea0d090bc38e7a98ceb2f0e633f3bf686831
Andre <andreaguirrec007@gmail.com>
2025-03-10 19:55:05 -0600

    2

--- portfolio-page/next.config.mjs
+++ portfolio-page/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    trailingSlash: true, // Opcional si usas rutas estáticas
+  };
+  
+  export default nextConfig;
+  
